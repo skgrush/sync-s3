7d48a47e8504422db8212d50f458ae850e3c728c
Samuel K. Grush <Samuel@Grush.org>
2025-02-14 21:04:44 -0600

    update json assert to with

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 import AjvModule from 'ajv';
 import { open } from 'node:fs/promises';
-import EnvSchema from './env.schema.json' assert { type: 'json' };
+import EnvSchema from './env.schema.json' with { type: 'json' };
 
 const Ajv = AjvModule.default;
 
